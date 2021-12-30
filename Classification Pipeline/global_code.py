0e7ae5b08828d538964790adb07fa53b99a11d5f
osamples <46390785+osamples@users.noreply.github.com>
2021-12-30 13:05:49 -0700

    Create global_code.py

---
+++ Classification Pipeline/global_code.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import spacy
+import string
+import numpy as np
+import random
+from spacy.util import minibatch, compounding
+from pandas.io.json import json_normalize
+#from spacy.pipeline.textcat import single_label_cnn_config
+#from spacy.training.example import Example
+
+#defining the function to remove punctuation
+def remove_punctuation(text):
+    punctuationfree="".join([i for i in text if i not in string.punctuation])
+    return punctuationfree
+
+def evaluate(tokenizer, textcat, texts, cats):
+    docs = (tokenizer(text) for text in texts)
+    tp = 0.0  # True positives
+    fp = 1e-8  # False positives
+    fn = 1e-8  # False negatives
+    tn = 0.0  # True negatives
+    for i, doc in enumerate(textcat.pipe(docs)):
+        gold = cats[i]
+        for label, score in doc.cats.items():
+            if label not in gold:
+                continue
+            if label == "NotSuspected":
+                continue
+            if score >= 0.7 and gold[label] >= 0.7:
+                tp += 1.0
+            elif score >= 0.7 and gold[label] < 0.5:
+                fp += 1.0
+            elif score < 0.5 and gold[label] < 0.5:
+                tn += 1
+            elif score < 0.5 and gold[label] >= 0.7:
+                fn += 1
+    precision = tp / (tp + fp)
+    recall = tp / (tp + fn)
+    if (precision + recall) == 0:
+        f_score = 0.0
+    else:
+        f_score = 2 * (precision * recall) / (precision + recall)
+    return {"textcat_p": precision, "textcat_r": recall, "textcat_f": f_score}
